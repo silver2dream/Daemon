ec831607fec336f2efa8ffa95e1703f263fb0ada
silver2dream <love3357671@gmail.com>
2018-05-09 20:48:14 +0800

    Create OneSingnal.js

---
+++ OneSingnal.js
@@ -0,0 +1,10 @@
+<link rel="manifest" href="/manifest.json" />
+<script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+<script>
+  var OneSignal = window.OneSignal || [];
+  OneSignal.push(function() {
+    OneSignal.init({
+      appId: "a2dff377-f795-458f-a80d-f2349fb3b295",
+    });
+  });
+</script>
